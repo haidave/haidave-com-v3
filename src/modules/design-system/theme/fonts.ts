10f230ec4674069a26118d5b86b7ab017c7db99d
Dave Nguyen <dave@haidave.com>
2024-11-10 00:36:14 +0100

    chore: project architecture

---
+++ src/modules/design-system/theme/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local'
+
+const geistMono = localFont({
+  src: '../../../app/fonts/GeistMonoVF.woff',
+  variable: '--font-mono',
+  weight: '100 900',
+})
+
+export { geistMono }
