b0cbd4fd83756e53ae2914a675322b19aa6d3bf2
Dave Nguyen <dave@haidave.com>
2024-11-10 22:52:03 +0100

    feat(design-system): add button

---
+++ src/modules/design-system/components/button.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { Slot, Slottable } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@/lib/utils'
+
+const buttonVariants = cva(
+  'group/button ease relative inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:shadow-focus focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-black font-semibold text-white dark:bg-white dark:text-black',
+        ghost: 'bg-transparent text-secondary hover:text-primary',
+      },
+      size: {
+        default: 'h-8 px-4 text-sm ',
+        icon: 'size-7',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        <Slottable>{props.children}</Slottable>
+      </Comp>
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
--- src/modules/design-system/components/theme-switcher.tsx
+++ src/modules/design-system/components/theme-switcher.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 import { MoonIcon, SunIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
+import { Button } from './button'
+
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
@@ -18,27 +20,17 @@ const ThemeSwitcher = () => {
 
   if (theme === 'dark') {
     return (
-      <button
-        onClick={() => setTheme('light')}
-        className="transition-colors hover:text-secondary"
-        aria-label="Set light mode"
-        type="button"
-      >
-        <SunIcon className="size-4" />
-      </button>
+      <Button variant="ghost" size="icon" onClick={() => setTheme('light')} aria-label="Set light mode" type="button">
+        <SunIcon className="size-4 stroke-[1.5]" />
+      </Button>
     )
   }
 
   if (theme === 'light') {
     return (
-      <button
-        onClick={() => setTheme('dark')}
-        className="transition-colors hover:text-secondary"
-        aria-label="Set dark mode"
-        type="button"
-      >
-        <MoonIcon className="size-4" />
-      </button>
+      <Button variant="ghost" size="icon" onClick={() => setTheme('dark')} aria-label="Set dark mode" type="button">
+        <MoonIcon className="size-4 stroke-[1.5]" />
+      </Button>
     )
   }
 }
