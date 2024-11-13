6d430413bf7ccc853709983e29d0944a0644e5cd
Dave Nguyen <dave@haidave.com>
2024-11-13 22:52:26 +0100

    feat(app): add metadata titles for all pages

--- src/app/craft/page.tsx
+++ src/app/craft/page.tsx
@@ -1,1 +1,8 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'craft',
+  description: 'a playground for sharpening my skills',
+}
+
 export { CraftPage as default } from '@/modules/craft/pages/craft'
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,1 +1,8 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'notes',
+  description: 'random thoughts and ideas',
+}
+
 export { NotesPage as default } from '@/modules/notes/pages/notes'
