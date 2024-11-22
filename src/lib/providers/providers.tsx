6895ce9197f098e00d68dfc7928b8d911b825e2b
Dave Nguyen <dave@haidave.com>
2024-11-23 00:53:46 +0100

    feat(providers): add ViewTransitions

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { ViewTransitions } from 'next-view-transitions'
+
 import { ThemeProvider } from './theme-provider'
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      {children}
-    </ThemeProvider>
+    <ViewTransitions>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        {children}
+      </ThemeProvider>
+    </ViewTransitions>
   )
 }
