a7552f94c34389b2965947fe2aa5b3a62ea28a21
Dave Nguyen <dave@haidave.com>
2024-11-26 22:11:31 +0100

    feat(footer): add email-button

--- src/config/links.ts
+++ src/config/links.ts
@@ -3,6 +3,7 @@ const LINKS = {
   x: 'https://x.com/haidave_',
   github: 'https://github.com/haidave',
   linkedin: 'https://www.linkedin.com/in/haidave/',
+  email: 'dave@haidave.com',
 
   // Work
   strv: 'https://www.strv.com',
---
+++ src/modules/design-system/components/email-button.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { CheckIcon, CopyIcon } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+
+import { LINKS } from '@/config/links'
+import { cn, copyToClipboard } from '@/lib/utils'
+
+import { Button } from './button'
+
+const EmailButton = () => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopy = async () => {
+    await copyToClipboard(LINKS.email)
+    setIsCopied(true)
+  }
+
+  useEffect(() => {
+    if (isCopied) {
+      const timeoutId = window.setTimeout(() => {
+        setIsCopied(false)
+      }, 2000)
+
+      return () => {
+        window.clearTimeout(timeoutId)
+      }
+    }
+  }, [isCopied])
+
+  const variants = {
+    hidden: { opacity: 0, scale: 0.5, transition: { duration: 0.15 } },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.15 } },
+  }
+
+  return (
+    <Button variant="ghost" size="link" onClick={handleCopy} className="group gap-1.5">
+      E-mail
+      <div className={cn('opacity-0 transition-opacity group-hover:opacity-100', isCopied && 'opacity-100')}>
+        <AnimatePresence mode="wait" initial={false}>
+          {isCopied ? (
+            <motion.span key="checkmark" variants={variants} initial="hidden" animate="visible" exit="hidden">
+              <CheckIcon size={14} />
+            </motion.span>
+          ) : (
+            <motion.span key="copy" variants={variants} initial="hidden" animate="visible" exit="hidden">
+              <CopyIcon size={14} className="opacity-100" />
+            </motion.span>
+          )}
+        </AnimatePresence>
+      </div>
+    </Button>
+  )
+}
+
+export { EmailButton }
